e88fd5763a416df9fbd5232dc8ac90ad4657f897
Farhan933 <farhanfarabiemon@gmail.com>
2023-10-23 23:19:42 +0600

    Banner done

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,9 @@ import AuthProvider from './providers/AuthProvider';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
     </AuthProvider>
   </React.StrictMode>,
 )
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,7 +4,6 @@ import Banner from "../Banner/Banner";
 const Home = () => {
     return (
         <div>
-            <h2>Home page</h2>
             <Banner></Banner>
         </div>
     );
